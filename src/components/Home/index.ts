1e9fd0f22fe187cf3b212f0e505dfe9a2dac483b
Joaquin1416 <jgalarce@interhaus.cl>
2026-02-16 16:33:02 -0300

    Landing page design

---
+++ src/components/Home/index.ts
@@ -0,0 +1,5 @@
+export { default as Navbar } from './Navbar.astro';
+export { default as Cards } from './Cards.astro';
+export { default as Footer } from './Footer.astro';
+export { default as Hero } from './Hero.astro';
+export { default as Plans } from './Plans.astro';
