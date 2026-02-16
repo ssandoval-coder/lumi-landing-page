07d57b58cc093be04ffab76f4bde9e527b30be0a
Joaquin1416 <jgalarce@interhaus.cl>
2026-02-16 16:59:03 -0300

    Languaje selection

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,22 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: 'es',
-        locales: ['es', 'en', 'pt'],
-        routing: {
-            prefixDefaultLocale: true,
-            redirectToDefaultLocale: false,
-        }
-    },
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  i18n: {
+      defaultLocale: 'es',
+      locales: ['es', 'en', 'pt'],
+      routing: {
+          prefixDefaultLocale: true,
+          redirectToDefaultLocale: false,
+      }
+  },
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [icon()],
 });
